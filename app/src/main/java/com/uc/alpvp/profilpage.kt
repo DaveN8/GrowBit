edcc61e0855b0a8d9a10d7b4dd12abc5f9b5bef0
pallas619 <mgathfan@student.ciputra.ac.id>
2023-01-03 00:06:06 +0700

    asede

---
+++ app/src/main/java/com/uc/alpvp/profilpage.kt
@@ -0,0 +1,11 @@
+package com.uc.alpvp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class profilpage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profilpage)
+    }
+}
